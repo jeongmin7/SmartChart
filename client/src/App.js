acc4a105270a22227050fd0c2a7751c24e1d05a7
jeongmin7 <lee.jeongmin1625@gmail.com>
2023-08-04 21:25:29 +0900

    Feat:분기 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,24 @@
+import DoctorSignUp from "./pages/DoctorSignUp";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PatientSignUp from "./pages/PatientSignup";
+import GlobalStyles from "./styles/GlobalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>초기설정</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register/doctor" element={<DoctorSignUp />} />
+          <Route path="/register/patient" element={<PatientSignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
