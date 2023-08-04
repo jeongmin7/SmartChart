140505e972cfd6be7b94b9fba6f1a8797ed47a09
jeongmin7 <lee.jeongmin1625@gmail.com>
2023-08-05 00:28:11 +0900

    Feat: 네브바, 홈, 낫파운드 페이지

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../components/Logo";
+
+const Home = () => {
+  return (
+    <>
+      <nav>
+        <Logo />
+      </nav>
+    </>
+  );
+};
+
+export default Home;
