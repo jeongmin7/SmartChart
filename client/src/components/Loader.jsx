16179d873e226d993b3aa461f4ac088b331ef019
hongwr <hongwr88@gmail.com>
2023-08-08 23:50:54 +0900

    edit loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { RotatingLines } from "react-loader-spinner";
+import { styled } from "styled-components";
+import { palette, zIndex } from "../styles/GlobalStyles";
 
 const Loader = ({ basic }) => {
   if (basic) {
@@ -14,18 +16,30 @@ const Loader = ({ basic }) => {
     </div>;
   }
   return (
-    <div>
-      <div>
-        <RotatingLines
-          strokeColor="grey"
-          strokeWidth="5"
-          animationDuration="0.75"
-          width="30"
-          visible={true}
-        />
-      </div>
-    </div>
+    <LoaderContainer>
+      <RotatingLines
+        strokeColor="#1798e1"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="200"
+        visible={true}
+      />
+    </LoaderContainer>
   );
 };
 
 export default Loader;
+
+const LoaderContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${palette.primary.black};
+  width: 100vw;
+  height: 100vh;
+  opacity: 0.5;
+  z-index: ${zIndex.modal};
+`;
