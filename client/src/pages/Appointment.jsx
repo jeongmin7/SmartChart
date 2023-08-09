a53bd9dcaa424dc870cadfd4d787e584a565337d
jeongmin7 <lee.jeongmin1625@gmail.com>
2023-08-10 01:37:51 +0900

    update: 날짜 선택과 시간선택 보충 & 데이터 불러오기 작업 남음

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import MapComponent from "../components/MapComponent";
+import SelectData from "../components/SelectData";
+import DatePicker from "../components/DatePickerComponent";
 
 const Appointment = () => {
+  const availableTimes = [
+    "09:00 - 10:00",
+    "10:00 - 11:00",
+    "11:00 - 12:00",
+    "12:00 - 13:00",
+    "13:00 - 14:00",
+    "14:00 - 15:00",
+    "15:00 - 16:00",
+    "16:00 - 17:00",
+    "17:00 - 18:00",
+  ];
+
   return (
     <div>
-      Appointment
-      <MapComponent />
+      <h1>병원 예약하기</h1>
+      <div>환자이름: </div>
+      <div>성별:</div>
+      <div>나이</div>
+      <div>전화번호</div>
+      <div>병원이름</div>
+      {/* 환자이름부터 병원이름까지는 저장된 데이터 리코일로 불러와야함 */}
+
+      <div>
+        예약날짜1
+        <DatePicker />
+      </div>
+      <SelectData availableOption={availableTimes} title="예약시간" />
     </div>
   );
 };
