b769da10ab587cac93c1feab71b2c9d2fb542010
hongwr <hongwr88@gmail.com>
2023-08-10 00:08:03 +0900

    edit signup button position

--- client/src/Container/SignUpContainer.jsx
+++ client/src/Container/SignUpContainer.jsx
@@ -4,13 +4,9 @@ import Button from "../components/Button";
 
 const SignUpContainer = () => {
   return (
-    <div>
+    <>
       <SignUpForm />
-      {/* <div>병원정보 검색하기</div> */}
-      <Button type="submit" width="80%">
-        회원가입
-      </Button>
-    </div>
+    </>
   );
 };
 
