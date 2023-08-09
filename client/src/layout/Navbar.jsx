09119875896ed1aaebc89b814e733bf6e88a9cc9
hongwr <hongwr88@gmail.com>
2023-08-10 00:07:07 +0900

    edit pathname error in navbar

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -5,7 +5,7 @@ import { styled } from "styled-components";
 
 const Navbar = () => {
   const pathname = useLocation().pathname;
-  if (pathname === "/" || pathname === "/signup" || pathname === " ") {
+  if (pathname === "/" || pathname === "/signUp" || pathname === " ") {
     return null;
   }
   return (
