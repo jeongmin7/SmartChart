e69156a6e3776ab253af33e4ef3dac0947323f4e
jeongmin7 <lee.jeongmin1625@gmail.com>
2023-08-10 12:37:51 +0900

    update: navbar logo일 경우 사이즈 조절

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { styled } from "styled-components";
 import smartLogo from "../assets/smartLogo.png";
 
-const Logo = ({ logo }) => {
-  return <Wrapper logo={logo}></Wrapper>;
+const Logo = ({ nav }) => {
+  return <Wrapper nav={nav}></Wrapper>;
 };
 
 export default Logo;
 
 const Wrapper = styled.div`
-  width: 520px;
-  height: 130px;
+  width: ${(props) => (props.nav ? "260px" : "520px")};
+  height: ${(props) => (props.nav ? "65px" : "130px")};
+
   background-color: yellow;
   background: url(${smartLogo}) no-repeat center center / 100% 100%;
 `;
