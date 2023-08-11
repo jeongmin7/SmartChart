fd87510e7e42238f0612adb9580fc8cb2ecd3158
jeongmin7 <lee.jeongmin1625@gmail.com>
2023-08-11 16:15:56 +0900

    update: 마이페이지 의사일경우와 환자일 경우 분기처리

--- client/src/Container/MypageContainer.jsx
+++ client/src/Container/MypageContainer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import MypageComponent from "../components/MypageComponent";
+import DoctorPage from "../components/DoctorPage";
+import { useRecoilValue } from "recoil";
+import { userInfoAtom } from "../stores/userInfo";
 
 const MypageContainer = () => {
+  const userInfo = useRecoilValue(userInfoAtom);
+  console.log(userInfo.isDoctor);
+  if (userInfo.isDoctor) {
+    return <DoctorPage />;
+  }
   return (
     <div>
       <MypageComponent />
