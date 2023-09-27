e4cbb56410911f4b2b4b5753f6484094092fd440
hongwr <hongwr88@gmail.com>
2023-09-28 01:23:15 +0900

    editing css

--- client/src/components/SalesTable.jsx
+++ client/src/components/SalesTable.jsx
@@ -4,7 +4,7 @@ import { palette } from "../styles/GlobalStyles";
 
 const SalesTable = () => {
   const columns = [
-    { name: "", width: "5%" },
+    { name: "11", width: "5%" },
     { name: "매출날짜", width: "20%" },
     { name: "환자수", width: "15%" },
     { name: "여성 환자 수", width: "15%" },
@@ -12,6 +12,9 @@ const SalesTable = () => {
     { name: "환자 평균 나이", width: "15%" },
     { name: "총 수입", width: "15%" },
   ];
+
+  //! 밑에 CSS 공사가 커진다. 이거 위에부터 싹 갈아야할듯;; 근데 위에 봤는데 차트랑 섞여서 어떻게 해야할지 감이 안 옴; 줄 안 보이는 거랑은 수정했는데 height가 좀 빡시다
+
   return (
     <ColumnHalfWrapper>
       <Header>이번 달 진료비 관리</Header>
@@ -61,7 +64,7 @@ const Header = styled.div`
 const ColumnHalfWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 70%;
+  width: 50%;
   height: auto;
   margin: 30px 0;
 `;
@@ -73,12 +76,15 @@ const AppointmentListTitle = styled.div`
   background-color: ${palette.gray.light};
   border-top: 1px solid ${palette.gray.border};
   border-bottom: 1px solid ${palette.gray.border};
+  border-right: 1px solid ${palette.gray.border};
 `;
+
 const AppointmentListBody = styled.div`
   display: flex;
   width: 100%;
   height: 20%;
   border-bottom: 1px solid ${palette.gray.border};
+  border-right: 1px solid ${palette.gray.border};
 `;
 
 const ListRowDivideWrapper = styled.div`
@@ -91,8 +97,10 @@ const ListRowDivideWrapper = styled.div`
   height: 100%;
   font-weight: bold;
   font-size: 16px;
-  border-left: ${(props) =>
-    props.index !== 0 && `1px solid ${palette.gray.border}`};
+  /* &:not(:first-child) {
+    border-left: 1px solid ${palette.primary.black};
+  } */
+  border-left: 1px solid ${palette.gray.border};
   padding-left: ${(props) =>
     props.index !== 0 && props.index !== 5 && props.index !== 6 && "10px"};
   background-color: ${(props) =>
