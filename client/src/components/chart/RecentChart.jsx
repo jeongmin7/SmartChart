0467f29882111732850504ee42df05970af11363
jeongmin7 <lee.jeongmin1625@gmail.com>
2023-10-04 15:51:55 +0900

    add: 최신순 차트

---
+++ client/src/components/chart/RecentChart.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const dailtrecent = [
+  {
+    sum: 400,
+    date: "2023-08-09",
+    patientCount: 1,
+  },
+  {
+    sum: 20000,
+    date: "2023-08-08",
+    patientCount: 1,
+  },
+  {
+    sum: 1600000,
+    date: "2023-08-04",
+    patientCount: 1,
+  },
+  {
+    sum: 140000,
+    date: "2023-07-09",
+    patientCount: 1,
+  },
+  {
+    sum: 40000,
+    date: "2023-07-08",
+    patientCount: 1,
+  },
+];
+
+const RecentChart = () => {
+  return <div>RecentChart</div>;
+};
+
+export default RecentChart;
