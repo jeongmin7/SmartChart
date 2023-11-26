8981c189cf2cb52039ac91c75054d3281fa96927
jeongmin <lee.jeongmin1625@gmail.com>
2023-11-27 00:46:10 +0900

    style:css 변경

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -4,7 +4,6 @@ import DatePicker from "../components/DatePickerComponent";
 import { styled } from "styled-components";
 import { palette } from "../styles/GlobalStyles";
 import Button from "../components/Button";
-import instance from "../components/api";
 import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { dateAtom, selectedOptionState } from "../stores/dateAtom";
@@ -149,43 +148,6 @@ const Appointment = () => {
 };
 
 export default Appointment;
-
-const AppointmentContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  /* background-color: yellow; */
-  width: 100vw;
-  height: calc(100vh - 100px);
-  min-width: 950px;
-  min-height: 800px;
-`;
-
-const AppointmentWrapper = styled.div`
-  /* background-color: green; */
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  align-items: center;
-  width: 40%;
-  height: 80%;
-  min-width: 950px;
-  min-height: 800px;
-  padding: 100px 200px 100px;
-  border: 1px solid ${palette.gray.border};
-  border-radius: 20px;
-  /* div + div {
-    margin-top: 20px;
-  } */
-`;
-
-// const Header = styled.div`
-//   background-color: red;
-//   font-weight: bold;
-//   margin-bottom: 20px;
-//   font-size: 25px;
-// `;
 
 const ColumnHalfWrapper = styled.div`
   display: flex;
