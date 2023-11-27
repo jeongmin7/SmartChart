6b2cb961f153ea440a23618a0739c9e95ef2bb36
jeongmin <lee.jeongmin1625@gmail.com>
2023-11-28 00:35:46 +0900

    update: 프록시 설정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
+    "/patient",
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_URL,
+      changeOrigin: true,
+    })
+  );
+  app.use(
     "/doctor",
     createProxyMiddleware({
       target: process.env.REACT_APP_API_URL,
@@ -10,18 +17,21 @@ module.exports = function (app) {
   );
 
   app.use(
-    "/patient",
-    createProxyMiddleware({
-      target: process.env.REACT_APP_API_URL,
-      changeOrigin: true,
-    })
-  );
-  app.use(
     "/ws/chat",
     createProxyMiddleware({
       target: process.env.REACT_APP_API_URL,
       changeOrigin: true,
       ws: true,
+    })
+  );
+  app.use(
+    "/auth/kakao/callback",
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_URL,
+      changeOrigin: true,
+      pathRewrite: {
+        "^/auth/kakao/callback": "/oauth2/authorization/kakao",
+      },
     })
   );
 };
