a3399cb988884c1f2dc60ff22050df9cdb5b8aad
jeongmin <lee.jeongmin1625@gmail.com>
2023-11-28 15:38:10 +0900

    fix: kakaoAuth 파일 요청 url 수정

--- client/src/pages/KakaoAuth.jsx
+++ client/src/pages/KakaoAuth.jsx
@@ -3,27 +3,21 @@ import React, { useEffect } from "react";
 
 const KakaoAuth = () => {
   const code = new URL(window.location.href).searchParams.get("code");
-  console.log("code", code);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.post("/auth/kakao/callback", code);
+        const response = await axios.post(
+          "http://localhost:3000/auth/kakao/callback",
+          { code }
+        );
         console.log(response);
       } catch (error) {
         console.error(error);
       }
     };
     fetchData();
-  }, []);
-  // useEffect(() => {
-  //   fetch("/auth/kakao/callback", {
-  //     method: "POST",
-  //     body: { code },
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded;",
-  //     },
-  //   });
-  // }, []);
+  }, [code]);
 
   return <div>KakaoAuth</div>;
 };
