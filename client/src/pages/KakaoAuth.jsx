d5ba6691c06b512b908b2455111d0ecac24c5b28
jeongmin <lee.jeongmin1625@gmail.com>
2023-11-28 21:10:34 +0900

    update: 배포된 주소로 변경

--- client/src/pages/KakaoAuth.jsx
+++ client/src/pages/KakaoAuth.jsx
@@ -1,17 +1,20 @@
 import axios from "axios";
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const KakaoAuth = () => {
   const code = new URL(window.location.href).searchParams.get("code");
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.post(
-          "http://localhost:3000/auth/kakao/callback",
+          "https://smartchart.vercel.app/auth/kakao/callback",
           { code }
         );
         console.log(response);
+        navigate("/mypage");
       } catch (error) {
         console.error(error);
       }
