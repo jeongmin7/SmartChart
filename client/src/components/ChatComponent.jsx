3195fb0d4a9b728e4d9a7a7733f577f1f270dae5
jeongmin7 <lee.jeongmin1625@gmail.com>
2023-12-03 00:12:34 +0900

    update: 웹소켓을 이용한 채팅

--- client/src/components/ChatComponent.jsx
+++ client/src/components/ChatComponent.jsx
@@ -1,111 +1,272 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { over } from "stompjs";
+import SockJS from "sockjs-client";
 import styled from "styled-components";
 import { palette } from "../styles/GlobalStyles";
-import { useNavigate } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { chatUsername } from "../stores/userInfo";
+import { Container, Header, Wrapper } from "../styles/CommonStyle";
 
-const ChatComponent = () => {
-  const navigate = useNavigate();
-  const [username, setUsername] = useRecoilState(chatUsername);
+var stompClient = null;
 
-  const handleUsernameSubmit = (event) => {
-    event.preventDefault();
-    if (username) {
-      navigate("/chatroom");
+const ChatApp = () => {
+  const [username, setUsername] = useState("");
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [connected, setConnected] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+
+  var colors = [
+    // 색상 배열
+    "#2196F3",
+    "#32c787",
+    "#00BCD4",
+    "#ff5652",
+    "#ffc107",
+    "#ff85af",
+    "#FF9800",
+    "#39bbb0",
+    "#b232b2",
+    "#8B4F1D",
+    "#EA813D",
+    "#FF5050",
+    "#FF895A",
+    "#FFA500",
+    "#FFE146",
+    "#FF28A7",
+    "#FFAAAF",
+    "#147814",
+    "#64D2D2",
+    "#2828CD",
+    "#1E82FF",
+    "#14D3FF",
+  ];
+
+  function getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+      hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
+  }
+
+  const connect = () => {
+    setIsConnecting(true);
+    let socket = new SockJS("/ws/chat");
+    stompClient = over(socket);
+    stompClient.connect({}, (frame) => {
+      setConnected(true);
+      setIsConnecting(false);
+      stompClient.subscribe("/topic/public", (sdkEvent) => {
+        onMessageReceived(sdkEvent);
+      });
+      stompClient.send(
+        "/app/chat.addUser",
+        {},
+        JSON.stringify({ sender: username, type: "JOIN" })
+      );
+    });
+  };
+
+  const sendMessage = () => {
+    if (stompClient) {
+      stompClient.send(
+        "/app/chat.sendMessage",
+        {},
+        JSON.stringify({
+          sender: username,
+          type: "CHAT",
+          content: currentMessage,
+        })
+      );
+      setCurrentMessage("");
     }
   };
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
+
+  const onMessageReceived = (payload) => {
+    let message = JSON.parse(payload.body);
+    setMessages((messages) => [...messages, message]);
   };
+
+  useEffect(() => {
+    return () => {
+      if (stompClient) {
+        stompClient.disconnect();
+      }
+    };
+  }, []);
+
   return (
-    <Background>
-      <Container>
-        <Wrapper>
-          <h1>Type your username </h1>
-          <h3>to enter the Chatroom </h3>
-          <form id="usernameForm" name="usernameForm">
-            <FormContainer>
-              <Input
-                type="text"
-                id="name"
-                placeholder="Username"
-                autoComplete="off"
-                className="form-control"
-                onChange={handleUsername}
-              />
-            </FormContainer>
-            <FormContainer>
-              <Button
-                type="submit"
-                className="accent username-submit"
-                onClick={handleUsernameSubmit}
-              >
-                Start Chatting
-              </Button>
-            </FormContainer>
-          </form>
-        </Wrapper>
-      </Container>
-    </Background>
+    <Container>
+      <Wrapper>
+        {!connected && !isConnecting && (
+          <ConnectionForm>
+            <input
+              type="text"
+              placeholder="닉네임을 입력하세요"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <button onClick={connect} disabled={!username}>
+              채팅 참여
+            </button>
+          </ConnectionForm>
+        )}
+        {isConnecting && <div>연결 중...</div>}
+        {connected && (
+          <ChatContainer>
+            <ChatContent>
+              <MessagesContainer>
+                {messages.map((msg, index) => (
+                  <MessageRow
+                    key={index}
+                    isOwnMessage={msg.sender === username}
+                  >
+                    <Avatar color={getAvatarColor(msg.sender)}>
+                      {msg.sender.charAt(0).toUpperCase()}
+                    </Avatar>
+                    <MessageContent isOwnMessage={msg.sender === username}>
+                      {msg.type === "JOIN" && (
+                        <div>{msg.sender}님이 입장하셨습니다.</div>
+                      )}
+                      {msg.type === "LEAVE" && (
+                        <div>{msg.sender}님이 떠나셨습니다.</div>
+                      )}
+                      {msg.type === "CHAT" && (
+                        <ChatText>{msg.content}</ChatText>
+                      )}
+                    </MessageContent>
+                  </MessageRow>
+                ))}
+              </MessagesContainer>
+              <InputContainer>
+                <MessageInput
+                  type="text"
+                  placeholder="메시지를 입력하세요"
+                  value={currentMessage}
+                  onChange={(e) => setCurrentMessage(e.target.value)}
+                />
+                <SendButton onClick={sendMessage}>전송</SendButton>
+              </InputContainer>
+            </ChatContent>
+          </ChatContainer>
+        )}
+      </Wrapper>
+    </Container>
   );
 };
 
-export default ChatComponent;
-const Container = styled.div`
-  text-align: center;
+export default ChatApp;
+
+const Avatar = styled.div`
+  height: 35px;
+  width: 35px;
+  background-color: ${(props) => props.color};
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  color: white;
+  font-weight: bold;
 `;
-const Wrapper = styled.div`
-  background: #fff;
-  box-shadow: 0 1px 11px rgba(0, 0, 0, 0.27);
-  border-radius: 2px;
-  width: 100%;
-  max-width: 600px;
-  display: inline-block;
-  margin-top: 42px;
-  vertical-align: middle;
-  position: relative;
-  padding: 35px 55px 35px;
-  min-height: 400px;
-  position: absolute;
-  top: 50%;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-  margin-top: -160px;
+const MessagesContainer = styled.div`
+  padding: 10px;
+  height: 400px;
+  overflow-y: auto;
 `;
-const FormContainer = styled.div`
-  margin-bottom: 15px;
+const MessageRow = styled.div`
   display: flex;
   align-items: center;
+  justify-content: ${(props) =>
+    props.isOwnMessage ? "flex-end" : "flex-start"};
+  margin-bottom: 10px;
+`;
+
+const MessageContent = styled.div`
+  max-width: 60%;
+  background: ${(props) =>
+    props.isOwnMessage ? `${palette.primary.blue}` : "#f0f0f0"};
+  color: ${(props) =>
+    props.isOwnMessage ? "#ffff" : `${palette.primary.black}`};
+
+  padding: 8px 12px;
+  border-radius: 12px;
+  margin-left: ${(props) => (props.isOwnMessage ? "10px" : "0")};
+  margin-right: ${(props) => (props.isOwnMessage ? "0" : "10px")};
+`;
+
+const ChatText = styled.div`
+  word-break: break-word;
+`;
+
+const ConnectionForm = styled.div`
+  width: 500px;
+  height: 300px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: white;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+
+  input {
+    margin-bottom: 10px;
+    padding: 8px;
+    font-size: 16px;
+    width: 100%;
+  }
+
+  button {
+    margin-top: auto;
+    font-size: 16px;
+    font-weight: 600;
+    padding: 15px;
+    background-color: ${palette.primary.blue};
+    color: white;
+    cursor: pointer;
+    width: 25%;
+    align-self: center;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    &:disabled {
+      background-color: #ccc; /* 비활성 상태 배경색 */
+      color: #666; /* 비활성 상태 텍스트 색상 */
+      cursor: not-allowed; /* 비활성 상태에서 커서 스타일 변경 */
+    }
+  }
 `;
-const Input = styled.input`
-  padding-left: 10px;
-  outline: none;
+const ChatContainer = styled.div`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ChatContent = styled.div`
+  width: 80%; /* 혹은 적절한 너비를 설정하세요 */
+`;
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
-  height: 40px;
+  margin-top: 20px; /* 상단 여백 조절 */
 `;
-const Button = styled.button`
-  border: 1px solid transparent;
-  outline: none;
-  line-height: 100%;
-  white-space: nowrap;
-  padding: 0.6rem 1rem;
-  border-radius: 2px;
-  cursor: pointer;
-  min-height: 38px;
+
+const MessageInput = styled.input`
+  flex: 1;
+  padding: 8px;
+  font-size: 16px;
+`;
+
+const SendButton = styled.button`
+  margin-left: 10px; /* 버튼과 입력창 사이 여백 조절 */
+  padding: 12px;
+  font-size: 16px;
   background-color: ${palette.primary.blue};
-  color: #fff;
-`;
-const Background = styled.div`
-  margin: 0;
-  padding: 0;
-  font-weight: 400;
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 1rem;
-  line-height: 1.58;
-  color: #333;
-  background-color: aliceblue;
-  height: 100%;
+  color: white;
+  cursor: pointer;
+  border: none;
 `;
