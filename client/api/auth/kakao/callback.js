3959b8115bb51db325c03301eb4ca39af731f916
jeongmin <lee.jeongmin1625@gmail.com>
2023-12-05 23:18:37 +0900

    fix: 서버리스 사용

---
+++ client/api/auth/kakao/callback.js
@@ -0,0 +1,12 @@
+module.exports = async (req, res) => {
+  const { query } = req;
+
+  let destination = `${process.env.REACT_APP_API_KAKAO_REDIRECT_URL}`;
+
+  if (query.code) {
+    destination = `${process.env.REACT_APP_API_KAKAO_URL}?code=${query.code}`;
+  }
+
+  res.writeHead(301, { Location: destination });
+  res.end();
+};
