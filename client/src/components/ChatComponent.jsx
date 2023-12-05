acb7ad98d34148accb0f4dbb9d0fa32118814f7c
jeongmin <lee.jeongmin1625@gmail.com>
2023-12-05 17:08:57 +0900

    update: 웹소켓 사용한 채팅

--- client/src/components/ChatComponent.jsx
+++ client/src/components/ChatComponent.jsx
@@ -109,3 +109,63 @@ const Background = styled.div`
   background-color: aliceblue;
   height: 100%;
 `;
+import React from "react";
+import { css, styled } from "styled-components";
+import { palette } from "../styles/GlobalStyles";
+
+const Button = ({
+  type,
+  secondary = false,
+  bgColor,
+  fgColor,
+  width,
+  height,
+  padding,
+  marginB,
+  fontSize,
+  disabled,
+  borderRadius,
+  alignSelf,
+  whiteSpace,
+  fontweight,
+  ...restProps
+}) => {
+  const style = {
+    backgroundColor: bgColor || "",
+    color: fgColor || "",
+    width: width || "",
+    height: height || "",
+    marginBottom: marginB || "",
+    padding: padding || "",
+    fontSize: fontSize || "",
+    borderRadius: borderRadius || "",
+    alignSelf: alignSelf || "",
+    whiteSpace: whiteSpace || "",
+    fontweight: fontweight || "",
+  };
+
+  return (
+    <ButtonS type={type} style={style} {...restProps} disabled={disabled} />
+  );
+};
+
+export default Button;
+
+const ButtonS = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${palette.primary.blue};
+  padding: 1rem;
+  color: ${palette.white};
+  border-radius: 15px;
+  font-weight: 600;
+  cursor: pointer;
+  ${(props) =>
+    props.disabled &&
+    css`
+      background-color: #ccc;
+      color: #999;
+      cursor: not-allowed;
+    `}
+`;
