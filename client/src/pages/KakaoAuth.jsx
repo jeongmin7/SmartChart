7dac97e309fbadde24028bdbb243cea49301110b
jeongmin <lee.jeongmin1625@gmail.com>
2023-12-11 17:22:15 +0900

    fix: kakao로그인

--- client/src/pages/KakaoAuth.jsx
+++ client/src/pages/KakaoAuth.jsx
@@ -3,21 +3,22 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const KakaoAuth = () => {
-  // const code = new URL(window.location.href).searchParams.get("code");
-  // const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get("code");
+  const navigate = useNavigate();
+  const url = process.env.REACT_APP_API_KAKAO_URL;
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.post("/auth/kakao/callback", code);
-  //       console.log(response);
-  //       navigate("/mypage");
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [code]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(url, code);
+        console.log(response);
+        navigate("/mypage");
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [code]);
 
   return <div>KakaoAuth</div>;
 };
